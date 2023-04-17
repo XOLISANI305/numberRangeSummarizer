f1f7dd9924f274949b2288be288bda43042637d2
XOLISANI305 <131044353+XOLISANI305@users.noreply.github.com>
2023-04-17 22:10:11 +0200

    Add files via upload

--- NumberRangeSummarizer.java
+++ NumberRangeSummarizer.java
@@ -1,4 +1,8 @@
-package numberRangeSummarizer;
-
+import java.util.Collection;
 public interface NumberRangeSummarizer {
+    //collect the input
+    Collection<Integer> collect(String input);
+
+    //get the summarized string
+    String summarizeCollection(Collection<Integer> input);
 }
